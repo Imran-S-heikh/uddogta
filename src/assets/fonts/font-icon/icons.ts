556b484b99b514cede60720763c703daa870cc45
Imran-s-heikh <imransheikhsadi@gmail.com>
2024-08-23 21:15:06 +0600

    feat: Font Changed to Ubuntu Mono and Add Record Entry Popup Updated

--- src/assets/fonts/font-icon/icons.ts
+++ src/assets/fonts/font-icon/icons.ts
@@ -1,21 +1,33 @@
 export type IconsId =
   | "box-arrow-right"
+  | "calendar-minus"
+  | "calendar-plus"
+  | "close"
   | "delete"
   | "expand-more";
 
 export type IconsKey =
   | "BoxArrowRight"
+  | "CalendarMinus"
+  | "CalendarPlus"
+  | "Close"
   | "Delete"
   | "ExpandMore";
 
 export enum Icons {
   BoxArrowRight = "box-arrow-right",
+  CalendarMinus = "calendar-minus",
+  CalendarPlus = "calendar-plus",
+  Close = "close",
   Delete = "delete",
   ExpandMore = "expand-more",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
   [Icons.BoxArrowRight]: "61697",
-  [Icons.Delete]: "61698",
-  [Icons.ExpandMore]: "61699",
+  [Icons.CalendarMinus]: "61698",
+  [Icons.CalendarPlus]: "61699",
+  [Icons.Close]: "61700",
+  [Icons.Delete]: "61701",
+  [Icons.ExpandMore]: "61702",
 };
--- src/pages/Record/components/CreateRecordPopup.tsx
+++ src/pages/Record/components/CreateRecordPopup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import ButtonRipple from "@/components/buttons/ButtonRipple.component";
 import Icon from "@/molecules/Icon.mole";
 import Backdrop from "@/components/backdrop/Backdrop.component";
@@ -13,6 +13,7 @@ import DropDown from "@/components/drop-down/DropDown.component";
 import SpinLoaderState from "@/components/spin/spin.atom";
 import { Filter, Entry } from "@/type";
 import { ActionType } from "@/types/app.type";
+import Hide from "@/molecules/Hide.mole";
 
 const CreateRecordPopup = () => {
   const [open, setOpen] = useState(false);
@@ -22,7 +23,7 @@ const CreateRecordPopup = () => {
   const setEntries = useSetRecoilState(UserRecordState([id, user?.uid]));
   const [value, setValue] = useState(0);
   const [title, setTitle] = useState("");
-  const [action, setAction] = useState(ActionType.EXPENSE);
+  const action = useRecoilValue(TabState);
   const startSpin = useSetRecoilState(SpinLoaderState);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -33,7 +34,7 @@ const CreateRecordPopup = () => {
   const resetState = () => {
     setValue(0);
     setTitle("");
-    setAction(ActionType.EXPENSE);
+    // setAction(ActionType.EXPENSE);
   };
 
   const handleClick = async () => {
@@ -49,6 +50,7 @@ const CreateRecordPopup = () => {
       setEntries((pre) => [...pre, { ...data, id: ref.id }]);
       startSpin(false);
       resetState();
+      setOpen(false);
     }
   };
 
@@ -66,14 +68,29 @@ const CreateRecordPopup = () => {
       </ButtonRipple>
       <Backdrop open={open} className="Jc(c) Ai(c)">
         <ClickAwayListener onClickAway={() => setOpen(false)}>
-          <div className="Bgc(#082032) P(2rem) Bdrs(1rem)">
+          <div className="Bgc(#082032) P(2rem) Bdrs(1rem) Pos(r)">
+            <button
+              className="Pos(a) Bgc(t) End(1.5rem) T(1.5rem) Op(.5):active"
+              onClick={() => setOpen(false)}
+            >
+              <Icon icon="close" className="Fz(2rem) C(#fff) Cur(p) Bgc(t)" />
+            </button>
+            <h2 className="Ta(c) Fz(2rem) Fw(b) Tt(c)">
+              <Hide
+                open={action === ActionType.INCOME}
+                fallback={<Icon icon="calendar-minus" className="Fz(2rem) " />}
+              >
+                <Icon icon="calendar-plus" className="Fz(2rem)" />
+              </Hide>
+              Add {action.toLowerCase()} Entry
+            </h2>
             <div className="D(f) Fxd(c) Mt(2rem)">
               <div className="D(f) Fxd(c) Gap(1rem) Fx(1)">
                 <p className="Fz(1.6rem) C(#96abbb)">Title:</p>
                 <input
                   value={title}
                   placeholder="title..."
-                  className="Fz(16px) Fx(1) Mih(4rem) Bdrs(4rem) Px(2rem)"
+                  className="Fz(16px) Fx(1) Mih(4rem) Bdrs(4rem) Px(2rem) Mt(.5rem)"
                   onChange={handleTitle}
                 />
               </div>
@@ -83,22 +100,22 @@ const CreateRecordPopup = () => {
                   <input
                     value={value}
                     placeholder="amount..."
-                    className="Fz(16px) Fx(1) H(4rem) Bdrs(4rem) Px(2rem)"
+                    className="Fz(16px) Fx(1) H(4rem) Bdrs(4rem) Px(2rem) Mih(4rem) Mt(.5rem)"
                     onChange={handleChange}
                   />
                 </div>
-                <div className="D(f) Fx(1)">
+                {/* <div className="D(f) Fx(1)">
                   <DropDown
                     onChange={setAction}
                     label="Action:"
                     className="W(100%)"
                     options={[ActionType.EXPENSE, ActionType.INCOME]}
                   />
-                </div>
+                </div> */}
               </div>
               <ButtonRipple
                 onClick={handleClick}
-                className="Mt(1rem) Fz(1.6rem) Bgc(#ff4c29) C(#fff) D(b) H(2rem) Px(3rem) H(4rem) Bdrs(4rem)"
+                className="Fz(1.6rem) Bgc(#ff4c29) C(#fff) D(b) H(2rem) Px(3rem) H(4rem) Bdrs(4rem) Mt(2rem)"
               >
                 SUBMIT
               </ButtonRipple>
